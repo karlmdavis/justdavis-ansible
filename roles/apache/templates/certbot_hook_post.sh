755077bbf362f3bf3d79077d53a64099a45326d4
Karl M. Davis <karl@justdavis.com>
2021-04-14 21:50:08 -0400

    Fix the Certbot/SSL config.

--- roles/apache/templates/certbot_hook_post.sh
+++ roles/apache/templates/certbot_hook_post.sh
@@ -11,16 +11,21 @@ set -o pipefail
 
 echo 'Certbot post-hook Script: start'
 
-# This is required for the ACL below to take effect.
-# The directory permissions ensure that it's safe to do.
-chmod o+r /etc/letsencrypt/live/{{ domain }}/privkey.pem
+if [ -f /etc/letsencrypt/live/{{ domain }}/privkey.pem ]; then
+  # This is required for the ACL below to take effect.
+  # The directory permissions ensure that it's safe to do.
+  chmod o+r /etc/letsencrypt/live/{{ domain }}/privkey.pem
 
-if [ $(getent group openldap) ]; then
-  echo 'The openldap group exists.'
-  setfacl -m g:openldap:rx /etc/letsencrypt/live
-  setfacl -m g:openldap:rx /etc/letsencrypt/archive
-  systemctl restart slapd
+  if [ $(getent group openldap) ]; then
+    echo 'The openldap group exists.'
+    setfacl -m g:openldap:rx /etc/letsencrypt/live
+    setfacl -m g:openldap:rx /etc/letsencrypt/archive
+    systemctl restart slapd
+    echo 'Restarted slapd.'
+  else
+    echo 'The openldap group does not exist.'
+  fi
 else
-  echo 'The openldap group does not exist.'
+  echo 'No certificates found.'
 fi
 echo 'Certbot post-hook Script: end'
